83712c84e55fbdc50d0fef26c733866cd313670c
HOIL <hoil20707@gmail.com>
2020-11-22 23:52:55 +0900

    asdf

--- TestProject/Form1.Designer.cs
+++ TestProject/Form1.Designer.cs
@@ -52,7 +52,6 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "메시지박스 출력";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
